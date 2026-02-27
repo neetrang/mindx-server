4a94e3c2c19e7a2f7d6ade60ab64db1c9f0f8377
neetrang <maitrang.071003@gmail.com>
2026-02-27 13:49:39 +0700

    hrh

--- utils/sendMail.ts
+++ utils/sendMail.ts
@@ -1,46 +1,35 @@
-require("dotenv").config();
-import nodemailer, { Transporter } from "nodemailer";
-import ejs from "ejs";
-import path from "path";
+require('dotenv').config()
+import nodemailer, {Transporter} from 'nodemailer'
+import ejs from 'ejs';
+import path from 'path';
 
-interface EmailOptions {
-  email: string;
-  subject: string;
-  template: string;
-  data: { [key: string]: any };
+interface EmailOptions{
+    email:string;
+    subject:string;
+    template:string;
+    data:{[key:string]:any};
 }
 
-const sendMail = async (options: EmailOptions): Promise<void> => {
+const sendMail=async (options:EmailOptions):Promise<void> => {
 
-  const transporter: Transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // QUAN TRỌNG cho Gmail trên Render
+    const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: true,
     auth: {
-      user: process.env.SMTP_MAIL,
-      pass: process.env.SMTP_PASSWORD,
+        user: process.env.SMTP_MAIL,
+        pass: process.env.SMTP_PASSWORD,
     },
-  });
-
-  const { email, subject, template, data } = options;
-
-  const templatePath = path.join(__dirname, "../mails", template);
-
-  const html: string = await ejs.renderFile(templatePath, data);
-
-  const mailOption = {
-    from: `"MindX" <${process.env.SMTP_MAIL}>`,
-    to: email,
-    subject,
-    html,
-  };
-
-  try {
+    });
+    const {email,subject,template,data} = options;
+    const templatePath = path.join(__dirname, '../mails',template);
+    const html:string = await ejs.renderFile(templatePath, data);
+    const mailOption = {
+        from: process.env.SMTP_MAIL,
+        to: email,
+        subject,
+        html
+    }
     await transporter.sendMail(mailOption);
-    console.log("✅ Email sent to:", email);
-  } catch (error) {
-    console.error("❌ SEND MAIL ERROR:", error);
-  }
-};
-
+}
 export default sendMail;
